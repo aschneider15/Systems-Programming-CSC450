86193733bd8692708b84c343bc084d065d66b82d
aschneider15 <69209408+aschneider15@users.noreply.github.com>
2023-03-13 22:39:29 -0500

    Final(?) Commit
    Implemented independent function "rotate(char *, char *, int)." Unfortunately, no matter how hard I tried I could not get the image to format properly when I rotate 90 degrees (see the code I commented out). Also I still couldn't figured out 270 degree rotation. I did my best. I have no regrets.

--- Homework 13/bmp.c
+++ Homework 13/bmp.c
@@ -1,12 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 unsigned int bitsToInt(char * bits);
+void rotate(char * path, char * newPath, int mode);
 
 int main(){
+    rotate("vader.bmp", "vader_rot_0.bmp", 0);
+    rotate("vader.bmp", "vader_rot_90.bmp", 1);
+    rotate("vader.bmp", "vader_rot_180.bmp", 2);
+    rotate("vader.bmp", "vader_rot_270.bmp", 3);
+    rotate("vader.bmp", "anakin.bmp", 4);
+    return 0;
+}
+
+void rotate(char * path, char * newPath, int mode){
+    char error;
     FILE * inputStream, * outputStream;
-    inputStream = fopen("vader.bmp", "rb");
-    outputStream = fopen("vader_mirrored.bmp", "wb");
+    inputStream = fopen(path, "rb");
+    outputStream = fopen(newPath, "wb");
 
     char intBuf[4];
 
@@ -42,12 +54,39 @@ int main(){
     for(int i = 0; i < 54; i++) {//write the header
         writeBuffer[i] = readBuffer[i];
     }
-    for(int y = 0; y < height; y++) {
-        for(int x = 0; x < width; x++) {
+/*
+    if(mode == 1 || mode == 3) {
+        for(int i = 0; i < 4; i++) {
+            writeBuffer[widthPos + i] = readBuffer[heightPos + i];
+            writeBuffer[heightPos + i] = readBuffer[widthPos + i];
+        }
+    }
+*/
+    for(unsigned int y = 0; y < height; y++) {
+        for(unsigned int x = 0; x < width; x++) {
             int oldPixel = y * width + x;
-            //0 degree rotation: int newPixel = y * width + (width - x - 1);
+            int newPixel;
+            switch(mode) {
+                case 0: //0 degree rot
+                    newPixel = y * width + x;
+                    break;
+                case 1: //90 degree rot
+                    newPixel = x * width + y;
+                    break;
+                case 2: //180 degree rot
+                    newPixel = (width * height) - ((width * y) + x);
+                    break;
+                case 3: //270 degree rot
+                    error = 1;
+                    break;
+                default:
+                    error = 2; //printf("Error: Invalid Mode\n");
+                    break;
+            }
+            //mirrored: int newPixel = y * width + (width - x - 1);
             //90 degree rotation: int newPixel = x * width + y;
             //180 degree rotation: int newPixel = (width * height) - ((width * y) + x);
+            //int newPixel = (width * height) - ();
             for(int i = 0; i < 3; i++) {
                 int oldIndex, newIndex;
                 oldIndex = 54 + oldPixel * 3 + i;
@@ -57,16 +96,24 @@ int main(){
         }
     }
 
+    switch(error) {
+            case 1:
+                printf("ERROR: 270 degree rotation has not been implemented yet.\n");
+                break;
+            case 2:
+                printf("ERROR: invalid mode.");
+                break;
+            default:
+                break;
+        }
+
     fwrite(writeBuffer, size, 1, outputStream);
 
     fclose(inputStream);
     fclose(outputStream);
     free(readBuffer);
     free(writeBuffer);
-
-    return 0;
 }
-
 
 unsigned int bitsToInt(char * bits){ //argument is 8 bit * 4 array
     //bits = char[4]{'A', 'B', 'C', 'D'};
